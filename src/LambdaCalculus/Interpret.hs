cba1695a73fbfe41d185d4ad15151a8dc26cec34
Caleb Stimpson <caleb.stimpson@gmail.com>
2026-07-26 11:16:37 -0600

    Interop IO of church strings

--- src/LambdaCalculus/Interpret.hs
+++ src/LambdaCalculus/Interpret.hs
@@ -29,12 +29,12 @@ evaluate scope = \case
   NFApplication nfFn arg -> evaluate scope nfFn `app` evaluate scope arg
   NFAbstraction param body -> L $ \arg -> evaluate (Map.insert param arg scope) body
 
-data IOKind = IOGetInt | IOPutInt | IOPure | IOFlatMap
+data IOKind = IOPure | IOFlatMap | IOGetInt | IOPutInt | IOGetLine | IOPutStrLn
   deriving (Eq, Ord, Show)
 data Intrinsic
   = IFn (Intrinsic -> Intrinsic)
   | INat Int
-  | IList [Int]
+  | IList [Value]
   | IIO IOKind [Value]
 instance Show Intrinsic where
   show (IFn _) = "<function>"
@@ -47,22 +47,29 @@ appIntrinsic _ _ = error "type error: expected function"
 
 intrinsics :: Scope
 intrinsics = Map.fromList
-  [ ("io_getInt", Intrinsic $ IIO IOGetInt [])
-  , ("io_putInt", L $ \int -> Intrinsic $ IIO IOPutInt [int])
-  , ("io_pure", L $ \v -> Intrinsic $ IIO IOPure [v])
+  [ ("io_pure", L $ \v -> Intrinsic $ IIO IOPure [v])
   , ("io_flatMap", L $ \ma -> L $ \f -> Intrinsic $ IIO IOFlatMap [ma, f])
+  , ("io_getInt", Intrinsic $ IIO IOGetInt [])
+  , ("io_putInt", L $ \int -> Intrinsic $ IIO IOPutInt [int])
+  , ("io_getLine", Intrinsic $ IIO IOGetLine [])
+  , ("io_putStrLn", L $ \str -> Intrinsic $ IIO IOPutStrLn [str])
   ]
 
 execute :: Value -> IO Value
 execute = \case
+  Intrinsic (IIO IOPure [v]) -> pure v
+  Intrinsic (IIO IOFlatMap [io, f]) -> do
+    execute . app f =<< execute io
   Intrinsic (IIO IOGetInt []) -> do
     toChurchInt . read <$> getLine
   Intrinsic (IIO IOPutInt [vint]) -> do
     L id <$ print (fromChurchInt vint)
-  Intrinsic (IIO IOPure [v]) -> pure v
-  Intrinsic (IIO IOFlatMap [io, f]) -> do
-    execute . app f =<< execute io
-  val -> error $ "umatched IO: " <> show val
+  Intrinsic (IIO IOGetLine []) -> do
+    toChurchList . fmap toChurchChar <$> getLine
+  Intrinsic (IIO IOPutStrLn [vline]) -> do
+    print vline
+    L id <$ putStrLn (fromChurchChar <$> fromChurchList vline)
+  val -> error $ "unmatched IO: " <> show val
 
 toChurchInt :: Int -> Value
 toChurchInt 0 = L $ const $ L id
@@ -70,8 +77,27 @@ toChurchInt n = L $ \f -> L $ \a -> f `app` (toChurchInt (n - 1) `app` f `app` a
 fromChurchInt :: Value -> Int
 fromChurchInt val = let
   subject = val
-    `app` Intrinsic (IFn $ \(INat n) -> INat (n + 1))
+    `app` L (\(Intrinsic (INat n)) -> Intrinsic (INat (n + 1)))
     `app` Intrinsic (INat 0)
   in case subject of
     Intrinsic (INat n) -> n
     evalled -> error $ "type error: was expecting int, got " <> show evalled
+
+toChurchChar :: Char -> Value
+toChurchChar = toChurchInt . fromEnum
+fromChurchChar :: Value -> Char
+fromChurchChar = toEnum . fromChurchInt
+
+toChurchList :: [Value] -> Value
+toChurchList [] = L $ const $ L id
+toChurchList (x:xs) = L $ \ifCons -> L $ \_ -> ifCons `app` x `app` toChurchList xs
+fromChurchList :: Value -> [Value]
+fromChurchList val = let
+  subject = (val
+    `app` L (\x -> L (\xs ->
+        Intrinsic $ IList (x : fromChurchList xs)
+      )))
+    `app` Intrinsic (IList [])
+  in case subject of
+    Intrinsic (IList list) -> list
+    evalled -> error $ "type error: was expecting list, got " <> show evalled
